12f6fb3d114c0d7a0716100b7ba3213e11d2fa9b
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-01-27 18:41:55 +0100

    Deleted obsolete files

--- db/migrate/20130809171331_add_ranking_product.rb
+++
@@ -1,9 +0,0 @@
-class AddRankingProduct < ActiveRecord::Migration
-  def up
-    Product.create(name: 'Ranking', text: 'Ranking')
-  end
-  
-  def down
-    Product.where(name: 'Ranking').first.destroy
-  end
-end
