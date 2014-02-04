8750789d810c778644691ad015965f00db2ccb37
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-02-04 14:09:42 +0100

    Adds API resource users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  match '/products/ranking' => 'product/ranking#index'
+  get '/products/ranking' => 'product/ranking#index'
   
   namespace :api, defaults: {format: 'json'} do
     scope module: :v1, path: 'v1' do
+      # TODO: remove manual routing when get rid of this resource using root UsersController
+      resources :users
       resources :rankings
       resources :ranking_items
       resources :user_ranking_items
