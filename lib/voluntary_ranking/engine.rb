05171c0be22634842d646c2503abb69ab1743ff4
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-02-04 14:11:07 +0100

    Include ranking module in user through config.to_prepare.

--- lib/voluntary_ranking/engine.rb
+++ lib/voluntary_ranking/engine.rb
@@ -2,9 +2,13 @@ module VoluntaryRanking
   class Engine < ::Rails::Engine
     config.autoload_paths << File.expand_path("../..", __FILE__)
     config.autoload_paths << File.expand_path("../../../app/models/concerns", __FILE__)
-    
+     
     initializer 'configure ember-rails ranking', before: 'ember_rails.setup_vendor' do
       config.handlebars.templates_root << 'voluntary_ranking/templates'
+    end
+    
+    config.to_prepare do
+      User.send :include, Concerns::Model::User::Ranking
     end
   end
 end
