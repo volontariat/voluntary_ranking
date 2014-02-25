17c04310ca1b38e29cf9c9897293064356ab003d
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-02-25 19:45:31 +0100

    Adds rspec for User#move_to_top_of_page

---
+++ dummy/spec/models/user_ranking_item_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe 'UserRankingItem' do
+  let(:user) { FactoryGirl.create(:user)}
+  
+  before :each do
+    @attributes = { adjective: 'best', negative_adjective: 'worst', topic: 'movie', scope: 'ever' }
+  end
+  
+  context '1 page available' do
+    it 'inserts item at 1st position' do
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 1', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 2', best: true, stars: 4)
+      user_ranking_item = user.add_ranking_item @attributes.merge(thing_name: 'Thing 3', best: true, stars: 3)
+      
+      user_ranking_item.move_to_top_of_page(1); user_ranking_item.reload
+      
+      user_ranking_item.position.should == 1
+      user_ranking_item.stars.should == 5
+      user_ranking_item.best.should == true
+    end
+  end
+  
+  context 'move 7th item to page 2 of 2' do
+    before :each do
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 1', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 2', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 3', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 4', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 5', best: true, stars: 5)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 6', best: true, stars: 5)
+      @user_ranking_item = user.add_ranking_item @attributes.merge(thing_name: 'Thing 7', best: true, stars: 3)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 8', best: true, stars: 3)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 9', best: true, stars: 3)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 10', best: true, stars: 3)
+      
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 11', best: false, stars: 2)
+      user.add_ranking_item @attributes.merge(thing_name: 'Thing 12', best: false, stars: 1)
+    end
+    
+    it 'inserts item at position 1 of page 2' do
+      @user_ranking_item.move_to_top_of_page(2); @user_ranking_item.reload
+      
+      @user_ranking_item.position.should == 11
+      @user_ranking_item.stars.should == 2
+      @user_ranking_item.best.should == false
+    end
+  end
+end
