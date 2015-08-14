e048d56055a1e421d572bebf46f7961b43bd2ccb
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-14 21:29:10 +0200

    refs #15 Require components.

--- app/assets/javascripts/voluntary_ranking/app.js
+++ app/assets/javascripts/voluntary_ranking/app.js
@@ -1,6 +1,7 @@
 //= require_tree ./mixins
 //= require_tree ./models
 //= require_tree ./controllers
+//= require_tree ./components
 //= require_tree ./views
 //= require_tree ./helpers
 //= require_tree ./templates
