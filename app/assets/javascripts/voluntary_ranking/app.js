c8d4c3b4c7bd3f52987c2c0dd27c3a2ac236fa96
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-09-03 12:11:32 +0200

    Don't require non existing directory components.

--- app/assets/javascripts/voluntary_ranking/app.js
+++ app/assets/javascripts/voluntary_ranking/app.js
@@ -1,7 +1,6 @@
 //= require_tree ./mixins
 //= require_tree ./models
 //= require_tree ./controllers
-//= require_tree ./components
 //= require_tree ./views
 //= require_tree ./helpers
 //= require_tree ./templates
